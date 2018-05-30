efbcd20872f279da7218aa9db10b24155f135b6c
JFT <masterjft@gmail.com>
2018-05-30 18:51:53 +0200

    replace lua with better version

--- Makefile.All.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set BUILD_ENGINE="Visual Studio 15 2017"
-if not [%1] == [] set BUILD_ENGINE=%1
-
-call Makefile.Win32.bat %BUILD_ENGINE%
-call Makefile.Win64.bat %BUILD_ENGINE%
-popd
--- android_build/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_MODULES := lua52
-APP_PLATFORM := android-15
-APP_OPTIM   := release
-APP_ABI := armeabi armeabi-v7a x86
